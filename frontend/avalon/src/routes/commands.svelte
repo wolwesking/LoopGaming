c8a8b9c8a5ed9a12f7ed20e96163b2bd3ee41547
wolwesking <83821890+wolwesking@users.noreply.github.com>
2023-12-20 15:54:13 +0500

    commandsWidgetDone

---
+++ frontend/avalon/src/routes/commands.svelte
@@ -0,0 +1,176 @@
+<script>
+	import { onMount } from 'svelte';
+
+	// Global var
+	let selectroLeftPosition = 11.296428680419922;
+	let text1Center = 0;
+	let text2Center = 0;
+	let text3Center = 0;
+	let text1Selected = 'selected';
+	let text2Selected = '';
+	let text3Selected = '';
+	let datasource = '';
+
+	const giveawayCommands = [
+		{ a: 'Create/Start', b: 'Start a giveaway in your guild!' },
+		{ a: 'Drop', b: 'Start a drop giveaway!' },
+		{ a: 'Edit', b: 'Edit an already running giveaway!' },
+		{ a: 'End', b: 'End an already running giveaway!' },
+		{ a: 'List', b: "List all the giveaway's running within this guild!" },
+		{ a: 'Pause', b: 'Pause an already running giveaway nh ' },
+		{ a: 'Reroll', b: 'Reroll an ended giveaway!' },
+		{ a: 'Resume', b: 'Resume a paused giveaway!' }
+	];
+
+	const generalCommands = [
+		{ a: 'Help', b: 'Shows all available commands to this bot!' },
+		{ a: 'Invite', b: 'Get the bots invite link!' },
+		{ a: 'Ping', b: 'Check the bots web-socket latency!' }
+	];
+
+	datasource = generalCommands.concat(giveawayCommands);
+
+	// default position
+
+	function calculateCenter() {
+		const All = document.getElementById('All');
+		if (All) {
+			const rect = All.getBoundingClientRect();
+			text1Center = rect.left + rect.width / 2 - 50;
+			selectroLeftPosition = text1Center;
+		}
+
+		const Giveaway = document.getElementById('Giveaway');
+		if (Giveaway) {
+			const rect = Giveaway.getBoundingClientRect();
+			text2Center = rect.left + rect.width / 2 - 50;
+		}
+		const General = document.getElementById('General');
+		if (General) {
+			const rect = General.getBoundingClientRect();
+			text3Center = rect.left + rect.width / 2 - 50;
+		}
+	}
+
+	// Button push handling
+	function focus(a) {
+		console.log();
+		switch (a) {
+			case 0:
+				selectroLeftPosition = text1Center;
+				text1Selected = 'selected';
+				text2Selected = '';
+				text3Selected = '';
+				datasource = generalCommands.concat(giveawayCommands);
+				break;
+			case 1:
+				selectroLeftPosition = text2Center;
+				text2Selected = 'selected';
+				text3Selected = '';
+				text1Selected = '';
+				datasource = giveawayCommands;
+				break;
+			case 2:
+				selectroLeftPosition = text3Center;
+				text3Selected = 'selected';
+				text2Selected = '';
+				text1Selected = '';
+				datasource = generalCommands;
+				break;
+		}
+	}
+
+	onMount(() => {
+		calculateCenter();
+	});
+</script>
+
+<header>
+	<button on:click={() => focus(0)}>
+		<p id="All" class={text1Selected} style="margin-right: 53.5px;">All</p>
+	</button>
+	<button on:click={() => focus(1)}>
+		<p id="Giveaway" class={text2Selected} style="margin-right: 28px;>">Giveaway</p>
+	</button>
+	<button on:click={() => focus(2)}>
+		<p id="General" class={text3Selected}>General</p>
+	</button>
+	<img src="/backgroundSelector.svg" alt="" style="left: {selectroLeftPosition}px;" />
+</header>
+<article>
+	<ul>
+		{#each datasource as item, index}
+			<li key={index}>
+				<span class="title">{item.a}</span>
+				<span class="greyTextStyle"> - {item.b}</span>
+			</li>
+		{/each}
+	</ul>
+</article>
+
+<style>
+	* {
+		font-family: Montserrat;
+	}
+	button {
+		/* Reset all default button styles */
+		appearance: none;
+		background: none;
+		color: #fff;
+		border: none;
+		margin: 0;
+		padding: 0;
+		font: inherit;
+		cursor: pointer;
+		outline: none; /* Remove the default focus outline */
+		/* Additional styles can be added or adjusted as needed */
+	}
+	header {
+		display: flex;
+		color: #fff;
+		padding-left: 50px;
+		font-family: Montserrat;
+		font-size: 18px;
+		font-style: normal;
+		line-height: normal;
+	}
+
+	header > img {
+		position: absolute;
+		top: 14.2px;
+		z-index: -1;
+		transition: left 0.2s ease; /* Apply transition to the left property */
+	}
+
+	.selected {
+		font-weight: 600;
+	}
+
+	.title {
+		color: #fff;
+		font-family: Montserrat;
+		font-size: 16px;
+		font-style: normal;
+		font-weight: 600;
+		line-height: normal;
+	}
+
+	.greyTextStyle {
+		color: #a7aea6;
+		font-size: 16px;
+		font-style: normal;
+		font-weight: 400;
+		line-height: normal;
+	}
+
+	ul {
+		list-style: none;
+		padding: 0; /* Optional: Remove default padding */
+		margin: 0;
+		/* Optional: Remove default margin */
+	}
+	li {
+		margin-top: 35px;
+		margin-bottom: 35px;
+	}
+</style>
--- frontend/avalon/src/routes/commands/+page.svelte
+++ frontend/avalon/src/routes/commands/+page.svelte
@@ -1,7 +1,12 @@
 <script>
 	import Header from "../header.svelte";
-
-
+	import Commands from "../commands.svelte";
 </script>
 
-<Header ListActive='true'/>
+<!-- <Header ListActive='true'/> -->
+<Header ListActive = 'true'/>
+<Commands />
+
+<style>
+	
+</style>
